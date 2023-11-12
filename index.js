7dd800b832d8b2b0e7ebccdd94e42ac11caf1769
joaocarlosweb <joaocarloswebcod@gmail.com>
2023-11-12 10:52:17 -0300

    Atualização Responsiva
    Atualizando Site Mario para responsivo.

---
+++ index.js
@@ -0,0 +1,12 @@
+let formulario = document.querySelector(".fale-conosco")
+    let mascara = document.querySelector(".mascara-form")
+    
+    function clicou(){
+        
+        formulario.style.left = "615px"
+       mascara.style.visibility="visible"
+    }
+    function sairForm(){
+        formulario.style.left = "-320px"
+        mascara.style.visibility="hidden"
+    }
